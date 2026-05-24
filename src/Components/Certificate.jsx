bc5010945f2a2b264e064a811c1bc17af147954f
SpicyTech2823 <sakirinsles@gmail.com>
2026-05-24 11:56:38 +0700

    Upadate certificate image

--- src/Components/Certificate.jsx
+++ src/Components/Certificate.jsx
@@ -1,43 +1,55 @@
 import OOAD from "../assets/images/OOAD.png";
 import stem2024 from "../assets/images/stem2024.png";
-import stem2025 from "../assets/images/STEM2025.png";
-import cro from "../assets/images/cro.png";
+import stem2025 from "../assets/images/stem2025.png";
+import cro from "../assets/images/Robotic.png";
+import Kangroo from "../assets/images/math_kangaroo.png";
 const Certificate = () => {
-    const certificates = [
-        {
-            id: 1,
-            Image: OOAD,
-        },
-        {
-            id: 2,
-            Image: stem2024,
-        },
-        {
-            id: 3,
-            Image: stem2025,
-        },
-        {
-            id: 4,
-            Image: cro,
-        }
-        
-
-        
-    ]
+  const certificates = [
+    {
+      id: 1,
+      Image: OOAD,
+    },
+    {
+      id: 2,
+      Image: stem2024,
+    },
+    {
+      id: 3,
+      Image: stem2025,
+    },
+    {
+      id: 4,
+      Image: cro,
+    },
+    {
+      id: 5,
+      Image: Kangroo,
+    },
+  ];
   return (
-    <section id='certificate' className='min-h-screen flex flex-col items-center justify-center text-white py-20'>
-        <h1 className='text-4xl font-bold text-center text-yellow-200 underline underline-offset-8 mb-10'>Certificate & Volunteer</h1>
-        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8 px-10'>
-            {certificates.map((certificate) => (
-                <div key={certificate.id} className='bg-gray-800 rounded-lg shadow-md overflow-hidden'>
-                    <img src={certificate.Image} alt={`Certificate ${certificate.id}`} className='w-[400px] h-auto' />
-                </div>
-            ))}
-        </div>
-
-        
+    <section
+      id="certificate"
+      className="min-h-screen flex flex-col items-center justify-center text-white py-20"
+    >
+      <h1 className="text-4xl font-bold text-center text-yellow-200 underline underline-offset-8 mb-10">
+        Certificate & Volunteer
+      </h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 px-10">
+        {certificates.map((certificate) => (
+          <div
+            key={certificate.id}
+            className="bg-gray-800 rounded-lg shadow-md overflow-hidden"
+          >
+            <img
+              src={certificate.Image}
+              alt={`Certificate ${certificate.id}`}
+              className="w-[400px] h-auto"
+            />
+          </div>
+        ))}
+      </div>
     </section>
-    )
-}
+  );
+};
 
 export default Certificate;
